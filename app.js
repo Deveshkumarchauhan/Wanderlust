3dd1339d0e47de6039bbf1a7834e3e7cbb1385ae
Devesh kumar chauhan <deveshchauhan3859@gmail.com>
2025-11-11 16:32:25 +0530

    This is page fom vaildations

--- app.js
+++ app.js
@@ -76,7 +76,6 @@ app.put("/listings/:id", async (req, res) => {
 app.delete("/listings/:id", async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
   res.redirect("/listings");
 });
 
