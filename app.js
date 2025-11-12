8713fdeddc38458e752ad1758cdebceaff997f36
Devesh kumar chauhan <deveshchauhan3859@gmail.com>
2025-11-12 23:11:10 +0530

    create the reviews page and reviews route

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressErroor = require("./utils/ExpressError.js");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js")
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 main()
@@ -103,14 +105,32 @@ app.put(
   })
 );
 
+// Delete route 
+
 app.delete(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
     res.redirect("/listings");
   })
 );
+
+// Reviews  
+// Post route 
+app.post("/listings/:id/reviews", async (req, res) => {
+let listing = await Listing.findById(req.params.id);
+let newReview = new Review(req.body.review);
+
+listing.reviews.push(newReview);
+
+await newReview.save();
+await listing.save();
+
+console.log("new review saved");
+res.send("new reviews saved");
+})
 
 // app.get("/textlisting", async (req, res) => {
 //   let sampleListing = new Listing({
