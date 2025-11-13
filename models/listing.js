ba7bc89a40ea42fc93c38014621829e3b7b848b4
Devesh kumar chauhan <deveshchauhan3859@gmail.com>
2025-11-13 17:43:39 +0530

    this is delete the listing and  delete was a all listings

--- models/listing.js
+++ models/listing.js
@@ -1,35 +1,41 @@
 const mongoose = require("mongoose");
-const review = require("./review");
+const Review = require("./review.js");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   image: {
-  filename: {
-    type: String,
-    default: "listingimage",
+    filename: {
+      type: String,
+      default: "listingimage",
+    },
+    url: {
+      type: String,
+      default:
+        "https://tse3.mm.bing.net/th/id/OIP.BktD7_xVPz33Zo6l79QFvAHaEh?pid=Api&P=0&h=180",
+      set: (v) =>
+        v === ""
+          ? "https://tse3.mm.bing.net/th/id/OIP.BktD7_xVPz33Zo6l79QFvAHaEh?pid=Api&P=0&h=180"
+          : v,
+    },
   },
-  url: {
-    type: String,
-    default:
-      "https://tse3.mm.bing.net/th/id/OIP.BktD7_xVPz33Zo6l79QFvAHaEh?pid=Api&P=0&h=180",
-    set: (v) =>
-      v === ""
-        ? "https://tse3.mm.bing.net/th/id/OIP.BktD7_xVPz33Zo6l79QFvAHaEh?pid=Api&P=0&h=180"
-        : v,
-  },
-},
 
   price: Number,
   location: String,
   country: String,
-  reviews:[
+  reviews: [
     {
       type: Schema.Types.ObjectId,
-      ref:"Review"
-    }
-  ]
+      ref: "Review",
+    },
+  ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
